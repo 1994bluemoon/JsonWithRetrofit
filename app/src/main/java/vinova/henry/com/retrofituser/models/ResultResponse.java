b37b54ff86beb32390c3e76a774d05367e00afbd
1994bluemoon <minhhoang94_bluemoon@yahoo.com.vn>
2018-01-31 17:04:08 +0700

    update

--- app/src/main/java/vinova/henry/com/retrofituser/models/ResultResponse.java
+++ app/src/main/java/vinova/henry/com/retrofituser/models/ResultResponse.java
@@ -1,4 +1,4 @@
-package vinova.henry.com.retrofituser.models.pojo;
+package vinova.henry.com.retrofituser.models;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
